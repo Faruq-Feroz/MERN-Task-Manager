6b0e45d8208a7e5c4792317abfbdd90c658b65f9
Faruq-Feroz <164517766+Faruq-Feroz@users.noreply.github.com>
2025-03-03 04:53:14 +0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const baseURL = import.meta.env.VITE_API_URL || 'https://mern-task-manager-tg7d.onrender.com/api';
 
 const api = axios.create({
   baseURL,
